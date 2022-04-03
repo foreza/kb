100d2496b439e5deea9584f6fa2bc7ed45ce671f
j <jforeza@gmail.com>
2022-04-02 18:34:33 -0700

    Create euler-18.js
    This was more difficult than expected. Had to look up a solution (which I still didn't understand)
    This should be the brute force approach.

---
+++ euler/euler-18.js
@@ -0,0 +1,56 @@
+var tree = [
+    [75],
+    [95,64],
+    [17,47,82],
+    [18,35,87,10],
+    [20,04,82,47,65],
+    [19,01,23,75,03,34],
+    [88,02,77,73,07,63,67],
+    [99,65,04,28,06,16,70,92],
+    [41,41,26,56,83,40,80,70,33],
+    [41,48,72,33,47,32,37,16,94,29],
+    [53,71,44,65,25,43,91,52,97,51,14],
+    [70,11,33,28,77,73,17,78,39,68,17,57],
+    [91,71,52,38,17,14,91,43,58,50,27,29,48],
+    [63,66,04,68,89,53,67,30,73,16,69,87,40,31],
+    [04,62,98,27,23,09,70,98,73,93,38,53,60,04,23]
+];
+
+
+// var tree = [
+//     [3],
+//     [7, 4],
+//     [2, 4, 6],
+//     [8, 5, 9,3]
+// ]
+
+function fromTheBottomNowWeHere(tree) {
+
+    var finalTree = tree;       // clone the tree
+
+    // repeat loop until we are at row 0, we'll mutate this copy of the input tree
+    // Start from the 2nd to the bottom row.
+    for (var i = finalTree.length - 2; i >= 0; --i) {
+        console.log("processing this row: ", finalTree[i]);
+
+        for (var j = 0; j <  finalTree[i].length; ++j) {
+            var leftVal = finalTree[i+1][j];
+            var rightVal = finalTree[i+1][j+1] 
+            if (leftVal > rightVal){
+                finalTree[i][j] += leftVal
+            } else {
+                finalTree[i][j] += rightVal
+            }
+
+        }
+
+    }
+
+    return finalTree;
+}
+
+
+var solutionTree = fromTheBottomNowWeHere(tree);
+var solution = solutionTree[0][0];
+
+console.log("Solution: ", solution);
